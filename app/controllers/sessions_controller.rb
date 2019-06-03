e8cfc7ed24a9ab10b4c0cbd3194e824a1f32cba3
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-03 20:35:45 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if session[:name]
+      redirect_to '/'
+    elsif params[:name].blank?
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete [:name] if sessions[:name]
+    redirect_to '/'
+  end
 end
